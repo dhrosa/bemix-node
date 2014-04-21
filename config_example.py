528c7068e0948f8c85fec375b619be0ce588a57e
Miles Steele <miles@milessteele.com>
2014-04-21 12:58:00 -0400

    update config_example

--- config_example.py
+++ config_example.py
@@ -1,8 +1,8 @@
 NODENAME = "g413"
 
 SERVER_NOSSL = "http://rum.mit.edu"
-
 # SSL is not required right now.
 # SERVER = "https://rum.mit.edu"
-# SECRET_USERNAME = "security-expert-isaac"
-# SECRET_PASSWORD = "leaves-no-stone-unturned"
+
+SECRET_USERNAME = "security-expert-isaac"
+SECRET_PASSWORD = "leaves-no-stone-unturned"
